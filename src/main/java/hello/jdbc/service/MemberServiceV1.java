7ee08e01d5d2b940f0dfa7119cf18bd588f3208f
halfbasement <anyang0710@naver.com>
2022-05-23 23:18:38 +0900

    트랜잭션 적용 #2

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -12,6 +12,7 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId , int money) throws SQLException {
+        //시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
@@ -29,4 +30,6 @@ public class MemberServiceV1 {
             throw new IllegalStateException("이체중 예외 발생");
         }
     }
+
+
 }
